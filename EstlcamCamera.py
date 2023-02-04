25c198afb535dc51be51575b210efd808ad170a4
joe288 <joe288@live.de>
2023-02-04 10:06:03 +0100

    save and load settings

--- EstlcamCamera.py
+++ EstlcamCamera.py
@@ -4,6 +4,7 @@ import VideoCapture
 import cv2
 import PIL.Image, PIL.ImageTk
 import time
+import json
 
 class App:
     def __init__(self, window, window_title, video_source=0):
@@ -13,7 +14,7 @@ class App:
 
         # open video source (by default this will try to open the computer webcam)
         self.vid = VideoCapture.MyVideoCapture(self.video_source)
-
+        
         streamFrame = Frame(window)
         streamFrame.pack(side = LEFT )
 
@@ -27,52 +28,54 @@ class App:
         imageFrame = LabelFrame(settingsFrame, text="Image")
         imageFrame.pack(fill="both", expand="yes")
 
-        self.cb_imageMirrorState = IntVar()
-        self.cb_imageMirrorState.set(1)
+        self.load()
+
         self.cb_imageMirror=tkinter.Checkbutton(imageFrame, text="mirror", variable=self.cb_imageMirrorState ,command=self.mirror)
         self.cb_imageMirror.pack(anchor=W) 
 
         self.lb_rotate = Label(imageFrame, text="Rotate:")
         self.lb_rotate.pack(anchor=W)
-        rotate_value = tkinter.StringVar(value=self.vid.imageRotate)
-        self.sb_rotate = tkinter.Spinbox(imageFrame, width=5, from_=0, to=360, increment=0.1, textvariable = rotate_value ,command=self.setRotate)
+
+        self.sb_rotate = tkinter.Spinbox(imageFrame, width=5, from_=0, to=360, increment=0.1, textvariable = self.rotate_value ,command=self.setRotate)
         self.sb_rotate.pack(anchor=W) 
 
         self.lb_skale = Label(imageFrame, text="Skale:")
         self.lb_skale.pack(anchor=W)
-        skale_value = tkinter.StringVar(value=self.vid.imageSkale)
-        self.sb_skale = tkinter.Spinbox(imageFrame, from_=1.0, to=5.0, width=5, increment=0.1, textvariable = skale_value ,command=self.setSkale)
+
+        self.sb_skale = tkinter.Spinbox(imageFrame, width=5, from_=1, to=5, increment=0.1, textvariable = self.skale_value ,command=self.setSkale)
         self.sb_skale.pack(anchor=W) 
 
         circleFrame = LabelFrame(settingsFrame, text="Circle")
         circleFrame.pack(fill="both", expand="yes")
 
-        self.cb_circleState = IntVar()
         self.cb_circle=tkinter.Checkbutton(circleFrame, text="enbale", variable=self.cb_circleState ,command=self.circle)
         self.cb_circle.pack(anchor=W) 
 
         self.lb_circle = Label(circleFrame, text="Size:")
         self.lb_circle.pack(anchor=W)
-        circle_value = tkinter.StringVar(value=self.vid.radius)
-        self.sb_circle = tkinter.Spinbox(circleFrame, from_=10, to=100, width=5, textvariable = circle_value ,command=self.setCircleDia)
-        self.sb_circle.config(state='disabled')
+     
+        self.sb_circle = tkinter.Spinbox(circleFrame, from_=10, to=100, width=5, textvariable = self.circle_value ,command=self.setCircleDia)
+        if not self.cb_circleState.get() == 1:
+            self.sb_circle.config(state='disabled')
         self.sb_circle.pack(anchor=W) 
 
         crossFrame = LabelFrame(settingsFrame, text="Cross")
         crossFrame.pack(fill="both", expand="yes")
 
-        self.cb_crossState = IntVar()
         self.cb_cross=tkinter.Checkbutton(crossFrame, text="enbale", variable=self.cb_crossState ,command=self.cross)
         self.cb_cross.pack(anchor=W) 
 
-        self.cb_crossDiagonalState = IntVar()
         self.cb_crossDiagonal=tkinter.Checkbutton(crossFrame, text="diagonal", variable=self.cb_crossDiagonalState ,command=self.crossDiagonal)
-        self.cb_crossDiagonal.config(state='disabled')
+        if not self.cb_crossState.get() == 1:
+            self.cb_crossDiagonal.config(state='disabled')
         self.cb_crossDiagonal.pack(anchor=W) 
 
         # Button that lets the user take a snapshot
         self.btn_snapshot=tkinter.Button(settingsFrame, text="Snapshot", width=20, command=self.snapshot)
         self.btn_snapshot.pack(anchor=tkinter.CENTER, expand=False)
+
+        self.btn_save=tkinter.Button(settingsFrame, text="Save", width=20, command=self.save)
+        self.btn_save.pack(anchor=tkinter.CENTER, expand=False)
 
         # After it is called once, the update method will be automatically called every delay milliseconds
         self.delay = 15
@@ -86,13 +89,13 @@ class App:
             self.vid.imageMirror = False
 
     def setRotate(self):
-        self.vid.imageRotate = float(self.sb_rotate.get())
+        self.vid.imageRotate = float(self.rotate_value.get())
 
     def setSkale(self):
-        self.vid.imageSkale = float(self.sb_skale.get())
+        self.vid.imageSkale = float(self.skale_value.get())
 
     def setCircleDia(self):
-        self.vid.radius= int(self.sb_circle.get())
+        self.vid.radius= int(self.circle_value.get())
 
     def circle(self):
         if not self.cb_circleState.get():
@@ -121,6 +124,54 @@ class App:
         ret, frame = self.vid.get_frame()
         if ret:
             cv2.imwrite("frame-" + time.strftime("%d-%m-%Y-%H-%M-%S") + ".jpg", cv2.cvtColor(frame, cv2.COLOR_RGB2BGR))
+
+    def save(self):
+        dictionary = {
+        "mirror": self.cb_imageMirrorState.get(),
+        "rotate": self.rotate_value.get(),
+        "scale": self.skale_value.get(),
+        "circleDia": self.circle_value.get(),
+        "circle": self.cb_circleState.get(),
+        "cross": self.cb_crossState.get(),
+        "crossDiagonal": self.cb_crossDiagonalState.get()
+        }
+ 
+        with open("settings.json", "w") as outfile:
+            json.dump(dictionary, outfile)
+
+    def load(self):
+        # JSON file
+        f = open ('settings.json', "r")
+        
+        # Reading from file
+        data = json.loads(f.read())
+        
+        self.cb_imageMirrorState = IntVar()
+        self.cb_imageMirrorState.set(data["mirror"])
+        self.mirror()
+
+        self.rotate_value = tkinter.StringVar(value = data["rotate"])
+        self.setRotate()
+
+        self.skale_value = tkinter.StringVar(value = data["scale"])
+        self.setSkale()       
+        
+        self.circle_value = tkinter.StringVar(value = data["circleDia"])
+        self.setCircleDia()
+
+        self.cb_circleState = IntVar()
+        self.cb_circleState.set(data["circle"])
+        self.vid.enabelCircle = self.cb_circleState.get()
+
+        self.cb_crossDiagonalState =IntVar()
+        self.cb_crossDiagonalState.set(data["crossDiagonal"])
+
+        self.cb_crossState = IntVar()
+        self.cb_crossState.set(data["cross"])
+        self.vid.enabelCross = self.cb_crossState.get()
+        self.vid.diagonalCross = self.cb_crossDiagonalState.get()
+        # Closing file
+        f.close()
 
     def update(self):
         # Get a frame from the video source
